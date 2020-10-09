8b6f20623a7dc175d6e23173655fba2adae0a8a6
sarah637 <72129260+sarah637@users.noreply.github.com>
2020-10-09 10:50:56 +0100

    course in progress

---
+++ app01-lab-classes/Course.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Course here.
+ *
+ * @author (sarah)
+ * @version (0.1)
+ */
+public; class Course
+    // instance variables - replace the example below with your own
+   private String title;"computing"
+  
+   private String code;"G4oo"
+    /**
+     * Constructor for objects of class Course
+     */
+    public Course()
+    {
+        // initialise instance variables
+        
+    }
+
+    
+}

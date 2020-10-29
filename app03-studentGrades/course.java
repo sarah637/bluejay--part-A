1857db867b6e6da31ee9ccf008559f7825043e51
sarah637 <72129260+sarah637@users.noreply.github.com>
2020-10-29 15:59:01 +0000

    Update course.java

--- app03-studentGrades/course.java
+++ app03-studentGrades/course.java
@@ -17,6 +17,8 @@ public class course
     private Module module3;
     private Module module4;
     
+    private int finalMark;
+    private String finalGrade;
     
     /**
      * Constructer for objects of class Course
@@ -27,21 +29,85 @@ public class course
         this. codeNo = codeNo;
         this. title = title;
         
-        module1 = new Module("programming concepts", "CO452");
+        this.finalMark = 0;
+        this.finalGrade = null;
+        
+        createModules();
     }
 
-    public void addModule(Module module, int moduleNo)
+    public void acreateModules();
     {
-    
+      module1 = new Module("programming concepts", "CO452");
+      moudle2 = new Module("programming concepts", "CO452");
+      module3 = new Module("programming concepts", "CO452");
+      module4 = new Module("proggraming concepts", "CO452");
     
     }
+    
+    public void addModule(Module module, int moduleNo);
+    {
+        if(moduelNo == 1)
+        {
+            this.module1 = module;
+        }
+    }
+    
+    public void setMarkI(int mark, String codeNo)
+    {
+        if(moduel1.getCodeNo() == codeNo)
+        {
+            module1.awardMark(mark);
+        }
+
+    }
+   
+    public void calculateFinalMark()
+    {
+        if(courseCompleted())
+           {
+            int totalMark = module1.getMark() + module2.getMark() + 
+                module3.geMark() + module4.getMark();
+                
+                
+                finalMark = totalMark / 4;
+                print();
+                
+                
+            }
+            else
+            {
+        
+            }      
+    }
+        
+    public boolean courseCompleted()
+    
+    {
+        if((moduel1.isCompleted()) && (module2.iCcompleted())) 
+        (module3.isCompleted())) && (module4 .isCompleted()))
+        {
+            return true;
+        }
+        else return false;
+    }
+    
     /**
-     * print course
-     * 
+     * prints out the details of a course
      */
-    public void print();
+    public void print()
     {
         // put your code here
-       system.out.println("course" + codeNo + " - " title); 
+        system.out.println("course" + codeNo + " - " + title); 
+    }
+    
+    public void printModules()
+    {
+        
+        //print all the module marks
+        
+        if(courseCompleted())
+        {
+            System.out.println("FinalMark =" + finalMark);
+        }
     }
 }
